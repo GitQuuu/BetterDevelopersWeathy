c5aa56ee4eae6b32ccebdbf92e0eba6124eec452
Qu <qu@peer-performance.com>
2025-01-24 20:06:59 +0100

    add remarks with correct syntax for &

--- Api/Controllers/WeatherController/GetWeatherData/WeatherController.cs
+++ Api/Controllers/WeatherController/GetWeatherData/WeatherController.cs
@@ -2,19 +2,24 @@
 
 namespace Api.Controllers.WeatherController;
 
+/// <summary>
+/// Endpoint for weather
+/// </summary>
 public partial class WeatherController
 {
     /// <summary>
-    /// 
+    /// Get weather forecast
     /// </summary>
-    /// <param name="query">Can be anything from city name, zipcode, latitude and longiu</param>
+    /// <param name="query">Can be anything from city name, zipcode, latitude and longitude</param>
     /// <param name="days"> number of days ahead to be forecasted</param>
     /// <param name="language">specify a preferred language</param>///
     /// <returns></returns>
     /// <remarks>
-    ///     GET /api/weather?query=56.1518,10.2064&days=7&language=dk
+    /// Example: GET /api/weather?query=56.1518,10.2064&amp;days=7&amp;language=dk <br />
+    /// Example: GET /api/weather?query=Aalborg&amp;days=2&amp;language=dk
     /// </remarks>
     [HttpGet]
+    [ProducesDefaultResponseType]
     public async Task<IActionResult> GetWeatherData(string query, int days, string language)
     {
         return await _weatherBll.GetWeatherDataAsync(query, days, language);
