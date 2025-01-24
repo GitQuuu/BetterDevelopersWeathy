6d577e1241cb5a1b5ff4b399e824bdd3a0e898a4
Qu <Quanvle@live.dk>
2025-01-24 09:27:06 +0100

    move away from homecontroller

---
+++ Api/Controllers/WeatherController/GetWeatherData/WeatherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.WeatherController;
+
+public partial class WeatherController
+{
+    [HttpGet]
+    public async Task<IActionResult> GetWeatherData(string city, int days, string language)
+    {
+        return await _weatherBll.GetWeatherDataAsync(city, days, language);
+    }
+}
