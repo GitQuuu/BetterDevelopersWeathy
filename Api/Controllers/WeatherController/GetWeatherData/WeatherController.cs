c6d431d499475ef2607e15f04980f3214221aa7b
Qu <qu@peer-performance.com>
2025-01-24 20:15:56 +0100

    update summaries for the GetWeatherData endpoint

--- Api/Controllers/WeatherController/GetWeatherData/WeatherController.cs
+++ Api/Controllers/WeatherController/GetWeatherData/WeatherController.cs
@@ -13,7 +13,11 @@ public partial class WeatherController
     /// <param name="query">Can be anything from city name, zipcode, latitude and longitude</param>
     /// <param name="days"> number of days ahead to be forecasted</param>
     /// <param name="language">specify a preferred language</param>///
-    /// <returns></returns>
+    /// <returns>
+    /// Returns an HTTP response with the weather forecast data. If successful, 
+    /// the response contains weather details in JSON format. If the request 
+    /// is invalid, a 400 Bad Request is returned.
+    /// </returns>
     /// <remarks>
     /// Example: GET /api/weather?query=56.1518,10.2064&amp;days=7&amp;language=dk <br />
     /// Example: GET /api/weather?query=Aalborg&amp;days=2&amp;language=dk
