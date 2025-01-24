e01091edc642a9bc3a4e7c5dfdfd982f6b990495
Qu <Quanvle@live.dk>
2025-01-24 10:25:19 +0100

    add summary to endpoint

--- Api/Controllers/WeatherController/GetWeatherData/WeatherController.cs
+++ Api/Controllers/WeatherController/GetWeatherData/WeatherController.cs
@@ -4,9 +4,19 @@ namespace Api.Controllers.WeatherController;
 
 public partial class WeatherController
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="query">Can be anything from city name, zipcode, latitude and longiu</param>
+    /// <param name="days"> number of days ahead to be forecasted</param>
+    /// <param name="language">specify a preferred language</param>///
+    /// <returns></returns>
+    /// <remarks>
+    ///     GET /api/weather?query=56.1518,10.2064&days=7&language=dk
+    /// </remarks>
     [HttpGet]
-    public async Task<IActionResult> GetWeatherData(string city, int days, string language)
+    public async Task<IActionResult> GetWeatherData(string query, int days, string language)
     {
-        return await _weatherBll.GetWeatherDataAsync(city, days, language);
+        return await _weatherBll.GetWeatherDataAsync(query, days, language);
     }
 }
