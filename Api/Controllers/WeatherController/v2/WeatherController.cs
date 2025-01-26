45c31bcf93f3bf1e9f441bfff0487d9d263c16b7
Qu <qu@peer-performance.com>
2025-01-26 19:19:06 +0100

    New endpoint for version 2

---
+++ Api/Controllers/WeatherController/v2/WeatherController.cs
@@ -0,0 +1,16 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.WeatherController.v2;
+
+[ApiController]
+[ApiVersion(2.0)]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class WeatherController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> TestEndpoint()
+    {
+        return Ok("Endpoint works for version 2.0");
+    }
+}
