55a02a821e8564ca5e8cf87c237f6fcda772f9ec
Qu <qu@peer-performance.com>
2025-01-27 08:51:30 +0100

    add test case for success operation

--- Tests/ResponseServiceTests/HandleResultAsyncTests.cs
+++ Tests/ResponseServiceTests/HandleResultAsyncTests.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Services;
 using Services.ResponseService;
 
 namespace Tests.ResponseServiceTests;
@@ -12,12 +15,47 @@ public class HandleResultAsyncTests
     {
         _responseService = new ResponseService();
     }
+    
+    private class TestDto  
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
 
     [Test]
-    public async Task HandleResultAsync_WhenServiceResultIsSuccess_ShouldReturnOk()
+    public async Task HandleResultAsync_WhenServiceResultIsSuccessWithBody_ShouldReturnOk()
     {
         // Arrange
+        var someResultDto = new TestDto { Id = 1, Name = "TestName" };
+        var result = new ServiceResult<TestDto>(true, HttpStatusCode.OK,"This is success", someResultDto);{}
+        
         // Act
+        var response = await _responseService.HandleResultAsync(result);
+        var okResult = response as OkObjectResult;
+        
         // Assert
+        Assert.That(response, Is.Not.Null);
+        Assert.That(response, Is.InstanceOf<OkObjectResult>());
+        Assert.That(response, Is.Not.InstanceOf<NoContentResult>());
+        Assert.That(okResult?.Value, Is.Not.Null);
+        Assert.That(okResult?.StatusCode, Is.EqualTo((int)HttpStatusCode.OK));
+    }
+    
+    [Test]
+    public async Task HandleResultAsync_WhenServiceResultIsSuccessWithoutBody_ShouldReturnNoContent()
+    {
+        // Arrange
+        var somePostDto = new TestDto { Id = 1, Name = "TestName" };
+        var result = new ServiceResult<TestDto>(true, HttpStatusCode.NoContent,"This is success with no content");{}
+        
+        // Act
+        var response = await _responseService.HandleResultAsync(result);
+        var okResult = response as NoContentResult;
+        
+        // Assert
+        Assert.That(response, Is.Not.Null);
+        Assert.That(response, Is.InstanceOf<NoContentResult>());
+        Assert.That(response, Is.Not.InstanceOf<OkObjectResult>());
+        Assert.That(okResult?.StatusCode, Is.EqualTo((int)HttpStatusCode.NoContent));
     }
 }   
