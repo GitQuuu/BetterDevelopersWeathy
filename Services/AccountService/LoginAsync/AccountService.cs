4b5267f36d479dce33176dc07af927e9dcd51b7a
Qu <qu@peer-performance.com>
2025-01-27 12:48:13 +0100

    reorder the loginAsync method flow

--- Services/AccountService/LoginAsync/AccountService.cs
+++ Services/AccountService/LoginAsync/AccountService.cs
@@ -8,22 +8,17 @@ public partial class AccountService
 {
     public async Task<ServiceResult<LoginResponseDto>> LoginAsync(LoginRequestDto dto)
     {
-        var user = await _userManagerService.FindByNameAsync(dto.Email);
-        if (user.Data is null)
-        {
-            return new ServiceResult<LoginResponseDto>(false, HttpStatusCode.Unauthorized, "Email or password is incorrect");
-        }
-
-        if (user.Data.EmailConfirmed is false)
-        {
-            return new ServiceResult<LoginResponseDto>(false, HttpStatusCode.Unauthorized, "Your account hasn't been activated yet. Please check your email inbox or spam");
-        }
-
         var result = await _signInManagerService.PasswordSignInAsync(dto.Email, dto.Password, false, false);
 
         if (result.Succeeded is false)
         {
             return new ServiceResult<LoginResponseDto>(false, HttpStatusCode.Unauthorized, "Email or password is incorrect");
+        }
+        
+        var user = await _userManagerService.FindByEmailAsync(dto.Email);
+        if (user.Data is null)
+        {
+            return new ServiceResult<LoginResponseDto>(false, HttpStatusCode.Unauthorized, "No such user");
         }
 
         var accessToken  = await GenerateAccessTokenAsync(user.Data);
