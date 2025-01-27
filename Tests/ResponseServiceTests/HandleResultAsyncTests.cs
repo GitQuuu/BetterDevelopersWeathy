d7749ea983f40ce91c606611405a2910b9a4bfd5
Qu <qu@peer-performance.com>
2025-01-27 01:03:09 +0100

    add test library to solution

---
+++ Tests/ResponseServiceTests/HandleResultAsyncTests.cs
@@ -0,0 +1,21 @@
+using Services.ResponseService;
+
+namespace Tests.ResponseServiceTests;
+
+[TestFixture]
+public class HandleResultAsyncTests
+{
+    private IResponseService _responseService;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _responseService = new ResponseService();
+    }
+
+    [Test]
+    public async Task HandleResultAsync_Success()
+    {
+        
+    }
+}
