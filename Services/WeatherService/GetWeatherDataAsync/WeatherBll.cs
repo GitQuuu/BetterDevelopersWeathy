9a741b5b3c2e64af832d6a1674a77c978c9362f7
Qu <Quanvle@live.dk>
2025-01-29 10:34:05 +0100

    method refactored

--- Services/WeatherService/GetWeatherDataAsync/WeatherBll.cs
+++ Services/WeatherService/GetWeatherDataAsync/WeatherBll.cs
@@ -8,29 +8,18 @@ public partial class WeatherBll
 {
     public async Task<IActionResult> GetWeatherDataAsync(string city, uint days, string language, CancellationToken ct)
     {
-        var referer = _httpContextAccessor.HttpContext.Request.Headers["Referer"].ToString();
+        var referer = _requestContextAccessorService.GetReferrer();
+        bool isAllowedReferrer = referer is "http://localhost:4200/" or "https://betterweathy.netlify.app/";
+        var nameIdentifier = _requestContextAccessorService.GetUserId();
 
-        if (referer is "http://localhost:4200/" or "https://betterweathy.netlify.app/")
+        if (!isAllowedReferrer && string.IsNullOrEmpty(nameIdentifier))
         {
-            var weatherApiResponse = await _weatherApiWebService.ForeCastAsync(city, days, language, ct);
-
-            var response = await _weatherService.HandleWeatherDataAsync(weatherApiResponse,ct);
-        
-            return await _responseService.HandleResultAsync(response);
+            return Unauthorized();
         }
-        else
-        {
-            var nameIdentifier = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (string.IsNullOrEmpty(nameIdentifier))
-            {
-                return Unauthorized();
-            }
-            var weatherApiResponse = await _weatherApiWebService.ForeCastAsync(city, days, language, ct);
 
-            var response = await _weatherService.HandleWeatherDataAsync(weatherApiResponse,ct);
-        
-            return await _responseService.HandleResultAsync(response);
-        }
-    
+        var weatherApiResponse = await _weatherApiWebService.ForeCastAsync(city, days, language, ct);
+        var response = await _weatherService.HandleWeatherDataAsync(weatherApiResponse, ct);
+
+        return await _responseService.HandleResultAsync(response);
     }
 }
